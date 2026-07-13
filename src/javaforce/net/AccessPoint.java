1775b85f16e78269395c53405e1f6b8662e1c702
Peter Quiring <pquiring@gmail.com>
2026-07-13 09:30:57 -0400

    add AccessPoint

---
+++ src/javaforce/net/AccessPoint.java
@@ -0,0 +1,14 @@
+package javaforce.net;
+
+/** Access Point.
+ *
+ * @author pquiring
+ */
+
+public class AccessPoint {
+  public String ssid;
+
+  public String toString() {
+    return "AccessPoint:" + ssid;
+  }
+}
