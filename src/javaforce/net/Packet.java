57c3c0d6754474ce8d9e68ebabba2e218a6c2622
Peter Quiring <pquiring@gmail.com>
2026-07-22 11:08:31 -0400

    Packet : add resetRead/resetWrite : adjust length during write...() methods : add setDefaultMTU()

--- src/javaforce/net/Packet.java
+++ src/javaforce/net/Packet.java
@@ -8,8 +8,14 @@ import javaforce.*;
  */
 
 public class Packet {
-  public Packet() {}
+
+  private static int mtu = 1500;
+
+  public Packet() {
+    data = new byte[mtu];
+  }
   public Packet(byte endian) {
+    data = new byte[mtu];
     this.endian = endian;
   }
   public Packet(byte[] data, int offset, int length) {
@@ -45,12 +51,18 @@ public class Packet {
   public long ts;
   /** Packet media key frame. */
   public boolean keyFrame;
-  
+
   /** Reset offset to start reading packet. */
-  public void reset() {
+  public void resetRead() {
     offset = 0;
   }
-  
+
+  /** Reset offset to start writing packet. */
+  public void resetWrite() {
+    offset = 0;
+    length = 0;
+  }
+
   /** Set Packet Endian.
    * @param value = javaforce.Endian.*
    */
@@ -58,16 +70,16 @@ public class Packet {
     if (value < Endian.min || value > Endian.max) return;
     endian = value;
   }
-  
+
   public byte readByte() throws Exception {
     if (offset >= length) throw new Exception("Buffer underflow");
     return data[offset++];
   }
-  
+
   public short readShort() throws Exception {
     if (offset + 2 > length) throw new Exception("Buffer underflow");
     short value;
-    if (endian == Endian.B) 
+    if (endian == Endian.B)
       value = (short)BE.getuint16(data, offset);
     else
       value = (short)LE.getuint16(data, offset);
@@ -78,7 +90,7 @@ public class Packet {
   public int readInt() throws Exception {
     if (offset + 4 > length) throw new Exception("Buffer underflow");
     int value;
-    if (endian == Endian.B) 
+    if (endian == Endian.B)
       value = BE.getuint32(data, offset);
     else
       value = LE.getuint32(data, offset);
@@ -89,7 +101,7 @@ public class Packet {
   public long readLong() throws Exception {
     if (offset + 8 > length) throw new Exception("Buffer underflow");
     long value;
-    if (endian == Endian.B) 
+    if (endian == Endian.B)
       value = BE.getuint64(data, offset);
     else
       value = LE.getuint64(data, offset);
@@ -98,7 +110,7 @@ public class Packet {
   }
 
   public void read(byte[] buf) throws Exception {
-    read(buf, 0, buf.length);    
+    read(buf, 0, buf.length);
   }
 
   public void read(byte[] buf, int buf_offset, int buf_length) throws Exception {
@@ -106,47 +118,67 @@ public class Packet {
     System.arraycopy(data, offset, buf, buf_offset, buf_length);
     offset += buf_length;
   }
-  
+
   public void writeByte(byte value) throws Exception {
-    if (offset + 1 > data.length) throw new Exception("Buffer overflow");    
+    if (offset + 1 > data.length) throw new Exception("Buffer overflow");
     data[offset++] = value;
+    if (offset > length) {
+      length = offset;
+    }
   }
 
   public void writeShort(short value) throws Exception {
-    if (offset + 2 > data.length) throw new Exception("Buffer overflow");    
-    if (endian == Endian.B) 
+    if (offset + 2 > data.length) throw new Exception("Buffer overflow");
+    if (endian == Endian.B)
       BE.setuint16(data, offset, value);
     else
       LE.setuint16(data, offset, value);
     offset += 2;
+    if (offset > length) {
+      length = offset;
+    }
   }
 
   public void writeInt(int value) throws Exception {
-    if (offset + 4 > data.length) throw new Exception("Buffer overflow");    
-    if (endian == Endian.B) 
+    if (offset + 4 > data.length) throw new Exception("Buffer overflow");
+    if (endian == Endian.B)
       BE.setuint32(data, offset, value);
     else
       LE.setuint32(data, offset, value);
     offset += 4;
+    if (offset > length) {
+      length = offset;
+    }
   }
 
   public void writeLong(long value) throws Exception {
-    if (offset + 8 > data.length) throw new Exception("Buffer overflow");    
-    if (endian == Endian.B) 
+    if (offset + 8 > data.length) throw new Exception("Buffer overflow");
+    if (endian == Endian.B)
       BE.setuint64(data, offset, value);
     else
       LE.setuint64(data, offset, value);
     offset += 8;
+    if (offset > length) {
+      length = offset;
+    }
   }
 
   public void write(byte[] buf) throws Exception {
-    write(buf, 0, buf.length);    
+    write(buf, 0, buf.length);
   }
-  
+
   public void write(byte[] buf, int buf_offset, int buf_length) throws Exception {
-    if (offset + buf_length > data.length) throw new Exception("Buffer overflow");    
+    if (offset + buf_length > data.length) throw new Exception("Buffer overflow");
     System.arraycopy(buf, buf_offset, data, offset, buf_length);
     offset += buf_length;
+    if (offset > length) {
+      length = offset;
+    }
+  }
+
+  public static void setDefaultMTU(int mtu) {
+    if (mtu < 1500 || mtu > 65536) return;
+    Packet.mtu = mtu;
   }
 
   public String toString() {
